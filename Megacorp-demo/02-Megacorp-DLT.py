9790c51655d0d6aa196e1ae56e5945447d36e077
nicholas.barretta@databricks.com <nicholas.barretta@databricks.com>
2021-12-07 23:56:32 +0000

    finished notebook 2

--- Megacorp-demo/02-Megacorp-DLT.py
+++ Megacorp-demo/02-Megacorp-DLT.py
@@ -18,12 +18,42 @@ import dlt
 from pyspark.sql.functions import *
 from pyspark.sql.types import *
 
-json_path = "/databricks-datasets/wikipedia-datasets/data-001/clickstream/raw-uncompressed-json/2015_2_clickstream.json"
+json_path = "/mnt/quentin-demo-resources/turbine/incoming-data"
+
 @dlt.table(
-  comment="The raw wikipedia clickstream dataset, ingested from /databricks-datasets."
+  comment="The raw turbine sensor data - unexploded JSON."
 )
-def clickstream_raw():
-  return (spark.read.json(json_path))
+def turbines_bronze():
+  return (spark.read.parquet(json_path))
+
+@dlt.table(
+  comment="The silver turbine sensor data - processed so that JSON is exploded into columns.",
+  table_properties={"delta.autoOptimize.autoCompact" : "true", "delta.autoOptimize.optimizeWrite" : "true"}
+)
+@dlt.expect_or_drop("idNotNegative", "ID > 0")
+def turbines_silver():
+  jsonSchema = StructType([StructField(col, DoubleType(), False) for col in ["AN3", "AN4", "AN5", "AN6", "AN7", "AN8", "AN9", "AN10", "SPEED", "TORQUE", "ID"]] + [StructField("TIMESTAMP", TimestampType())])
+  df = spark.readStream.option("ignoreChanges", "true").table('dec21_flightschool_team2.turbines_bronze') \
+       .withColumn("jsonData", from_json(col("value"), jsonSchema)) \
+       .select("jsonData.*")
+  return df
+
+@dlt.table(
+  comment="The gold turbine status data."
+)
+@dlt.expect_or_drop("idNotNegative", "ID > 0")
+def turbines_status_gold():
+  return (spark.read.parquet('/mnt/quentin-demo-resources/turbine/status'))
+
+@dlt.table(
+  comment="Turbine sensor data joined with status - gold table.",
+  table_properties={"delta.autoOptimize.autoCompact" : "true", "delta.autoOptimize.optimizeWrite" : "true"}
+)
+def turbines_gold():
+  turbine_stream = spark.readStream.table('dec21_flightschool_team2.turbines_silver')
+  turbine_status = spark.read.table("dec21_flightschool_team2.turbines_status_gold")
+  df = turbine_stream.join(turbine_status, ['id'], 'left')
+  return df
 
 # COMMAND ----------
 
